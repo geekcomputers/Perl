5bb13481e451a6da88280a3b658e199ebf9b23ea
Craig Richards <craig@geekcomputers.co.uk>
2012-02-27 21:28:16 +0000

    Ping a Subnet

---
+++ ping.pl
@@ -0,0 +1,21 @@
+#!/usr/local/bin/perl
+
+# Script Name   : ping.pl
+
+# Author        : Craig Richards
+# Created       : 17th December 2007
+
+# Version       : 1.0
+
+# Modifications :
+
+# Description   : Ping a certain subnet, ping all addresses
+
+# Instructions  :
+
+$subnet = 000;
+while ($subnet <= 255) {
+  system("ping 170.198.42.$subnet");
+$subnet = $subnet + 1;}
+
+## End of Script

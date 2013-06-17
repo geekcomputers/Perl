f8abd2b14a8742e101a077020dc0e7da2e03b8f9
geekcomputers <craig@geekcomputers.co.uk>
2013-06-17 21:39:17 +0100

    Shows when the database was opened by looking at the alert log

--- database_open.pl
+++ database_open.pl
@@ -1,7 +1,6 @@
 #!/usr/local/bin/perl -w
 
-# Description : This is the program to analyze the alert log
-#               file and print out diagnostics.
+# Description : This is the program to analyze the alert log file and print out diagnostics.
 # Developer   : Manoj Murumkar
 # Date        : 5/29/2001
 

b682fe020f83fdf30dbc47821505dbf324551084
geekcomputers <craig@geekcomputers.co.uk>
2013-06-17 21:39:27 +0100

    Asks for a hexadecimal number and converts to decimal, then converts an octal to decimal

--- hextooct.pl
+++ hextooct.pl
@@ -1,19 +1,14 @@
 #!/usr/local/bin/perl -w
 
-# Script Name   : hextooct.pl
+# Script Name		: hextooct.pl
+# Author				: Craig Richards
+# Created				: 17th December 2007
+# Last Modified		: 
+# Version				: 1.0
 
-# Author        : Craig Richards
-# Created       : 17th December 2007
+# Modifications		: 
 
-# Version       : 1.0
-
-# Modifications :
-
-# Description   : Asks for a hexadecimal number and converts to decimal, then converts an octal to decimal
-#		  i.e. FF is 255 for hex 377 octal is 255 decimal
-
-# Instructions  :
-
+# Description			: Asks for a hexadecimal number and converts to decimal, then converts an octal to decimal i.e. FF is 255 for hex 377 octal is 255 decimal
 
 use strict;
 

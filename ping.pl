e99f3e46795bb5f69b3e902e615d867173aca12f
geekcomputers <craig@geekcomputers.co.uk>
2013-06-17 21:39:38 +0100

    Ping a certain subnet, ping all addresses

--- ping.pl
+++ ping.pl
@@ -1,17 +1,14 @@
 #!/usr/local/bin/perl
 
-# Script Name   : ping.pl
+# Script Name		: ping.pl
+# Author				: Craig Richards
+# Created				: 17th December 2007
+# Last Modified		: 
+# Version				: 1.0
 
-# Author        : Craig Richards
-# Created       : 17th December 2007
+# Modifications		: 
 
-# Version       : 1.0
-
-# Modifications :
-
-# Description   : Ping a certain subnet, ping all addresses
-
-# Instructions  :
+# Description			: Ping a certain subnet, ping all addresses
 
 $subnet = 000;
 while ($subnet <= 255) {
